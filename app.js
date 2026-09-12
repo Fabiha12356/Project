a3fabfe0a06e6b69e7df7ff7ec3664d0f2edb886
fabihasohail <msohailfabiha@gmail.com>
2026-09-12 16:07:02 +0500

    logIn/signup pages!

---
+++ app.js
@@ -0,0 +1,11 @@
+let SignUpBTn = document.querySelector("#SignUpBTn");
+let  logInBTn = document.querySelector("#logInBTn");
+
+
+SignUpBTn.addEventListener("click" , () =>{
+    window.location.href = "./signup.html"
+})
+
+logInBTn.addEventListener("click" , () =>{
+    window.location.href = "./logIn.html"
+})
